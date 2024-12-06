2e6bab0af772de68d4523c9f099d2dda695ce774
abhishekdhakar475 <abhishekdhakar475@gmail.com>
2024-12-06 04:30:45 -0800

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter your age date\n");
+	scanf("%d",&a);
+	int b;
+	printf("enter your age year\n");
+	scanf("%d",&b);
+	
+	printf("date: %d\n",a);
+	printf("year: %d\n",b);
+	
+	
+	
+}
